cb8d205bb3cc0cdcca993ba051660deaecda8f88
sumx21t <otoro180yen@gmail.com>
2024-12-11 23:44:27 +0900

    fix metrics

--- training/__init__.py
+++ training/__init__.py
@@ -1,3 +1,3 @@
 from .trainer import Trainer
 from .gan_trainer import GanTrainer
-from .metrics import JaccardIndex, AUC, PixelWiseAccuracy, NSS, Precision, Recall, F1Score, MSE, MAE
+from .metrics import JaccardIndex, RocAucMetric, PixelWiseAccuracy, Precision, Recall, F1Score, MSE, MAE
