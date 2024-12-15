cc4c66291838b40ba556d42a028fee35ef2df784
sumx21t <otoro180yen@gmail.com>
2024-12-16 01:30:24 +0900

    add normalize01

--- training/utils.py
+++ training/utils.py
@@ -12,3 +12,14 @@ def init_seed(seed):
     cuda.manual_seed_all(seed)
     backends.cudnn.deterministic = True
     backends.cudnn.benchmark = False
+
+
+def normalize01(image):
+    min_val = torch.min(image)
+    max_val = torch.max(image)
+
+    # 分母が 0 になる場合の処理
+    if max_val == min_val:
+        return torch.zeros_like(image)
+
+    return (image - min_val) / (max_val - min_val)
