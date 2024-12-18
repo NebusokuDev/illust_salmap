28671fc0dade2e97bceec87f8c53b4500bc39250
sumx21t <otoro180yen@gmail.com>
2024-12-18 18:06:43 +0900

    fix utils.py

--- training/utils.py
+++ training/utils.py
@@ -3,7 +3,12 @@ from pathlib import Path
 
 import numpy.random
 import torch
+from pytorch_lightning import Trainer
 from torch import cuda, backends
+from torch.nn import Module
+
+from models.dummy_net import DummyNet
+from models.saliency_model import SaliencyModel
 
 
 def init_seed(seed):
@@ -14,9 +19,13 @@ def init_seed(seed):
     backends.cudnn.deterministic = True
     backends.cudnn.benchmark = False
 
+def get_class_name(obj: object):
+    return type(obj).__name__
 
-def get_save_path(root, dataset, model):
-    return Path(f"{root}/{dataset}/{model}")
+
+def get_save_path(root: str | Path, dataset, model: Module):
+    model_name = get_class_name(model)
+    return Path(f"{root}/{dataset}/{model_name}")
 
 
 def get_log_path(root, dataset, model):
@@ -25,3 +34,6 @@ def get_log_path(root, dataset, model):
 
 def get_checkpoint_path(root, dataset, model):
     return get_save_path(root, dataset, model) / "checkpoint"
+
+if __name__ == '__main__':
+    print(get_save_path("./path", "to", DummyNet()))
