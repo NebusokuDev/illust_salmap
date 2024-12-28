8714012d28ac2d466ef8c0d28d502a76a1b97e6b
sumx21t <otoro180yen@gmail.com>
2024-12-29 00:38:19 +0900

    Update TensorBoard image logging and optimize buffer handling
    Specified the `dataformats` parameter for TensorBoard image logging to ensure proper image formatting. Refactored buffer handling in utility functions using a context manager for better resource management and code clarity.

--- illust_salmap/models/pspnet.py
+++ illust_salmap/models/pspnet.py
@@ -1,40 +1,64 @@
 import torch
-from torch.nn import Module, Conv2d, BatchNorm2d, Sequential, AvgPool2d, Upsample, ReLU, ModuleList, AdaptiveAvgPool2d
+from torch.nn import Module, Conv2d, BatchNorm2d, Sequential, Upsample, ReLU, AdaptiveAvgPool2d, \
+    MaxPool2d, ConvTranspose2d, Dropout2d, MSELoss
 from torchinfo import summary
 from torchvision.models import resnet50, ResNet50_Weights
 
 
 class PSPNet(Module):
-    def __init__(self):
+    def __init__(self, in_channels=3, num_classes=1):
         super().__init__()
-        self.feature_map = FeatureMap()
-        self.upsample = Upsample()
+        self.feature_map = FeatureMap(in_channels=in_channels)
         self.pyramid_pool = PyramidPool()
         self.aux_loss = AUXLoss()
-        self.decoder = Decoder()
+        self.decoder = Decoder(num_classes=num_classes)
 
     def forward(self, x):
+        x, tmp = self.feature_map(x)
+        x = self.pyramid_pool(x)
+        x = self.decoder(x)
+
         if self.training:
-            aux = self.aux_loss(x)
+            aux = self.aux_loss(tmp)
             return x, aux
 
         return x
 
 
 class FeatureMap(Module):
-    def __init__(self):
+    def __init__(self, in_channels=3):
         super().__init__()
+
         backbone = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
+
         self.layer0 = Sequential(
-            Conv2d(3, 64, 3, 2, 1),
+            Conv2d(in_channels, 64, 3, 2, 1),
             BatchNorm2d(64),
+            ReLU(),
+            Conv2d(64, 64, 3, 1, 1),
+            BatchNorm2d(64),
+            ReLU(),
+            Conv2d(64, 128, 3, 1, 1),
+            BatchNorm2d(128),
+            ReLU(),
+            MaxPool2d(3, 2, 1)
         )
+
         self.layer1 = backbone.layer1
+        self.layer1[0].conv1 = Conv2d(in_channels=128, out_channels=64, kernel_size=1, stride=1, bias=False)
+        self.layer1[0].downsample[0] = Conv2d(in_channels=128, out_channels=256, stride=1, kernel_size=1, bias=False)
         self.layer2 = backbone.layer2
-        self.layer3 = backbone.layer3
-        self.layer4 = backbone.layer4
-        self.avgpool = backbone.avgpool
-        self.fc = backbone.fc
+        self.layer3 = self.mod_conv(backbone.layer3, dilation=(2, 2), padding=(2, 2), stride=(1, 1))
+        self.layer4 = self.mod_conv(backbone.layer4, dilation=(4, 4), padding=(4, 4), stride=(1, 1))
+
+    def mod_conv(self, layer: Module, *, dilation: tuple[int, int], padding: tuple[int, int], stride: tuple[int, int]):
+        for name, module in layer.named_modules():
+            if "conv2" in name:
+                module.dilation, module.padding, module.stride = dilation, padding, stride
+            elif "downsample.0" in name:
+                module.stride = stride
+
+        return layer
 
     def forward(self, x):
         x = self.layer0(x)
@@ -66,7 +90,7 @@ class PyramidPool(Module):
 
 
 class PyramidPoolBlock(Module):
-    def __init__(self, output_size, in_channels=2048, out_channels=512, size=(60, 60)):
+    def __init__(self, output_size, in_channels=2048, out_channels=512, size=(64, 64)):
         super().__init__()
         self.avg_pool = AdaptiveAvgPool2d(output_size)
         self.conv = Conv2d(in_channels, out_channels, 1)
@@ -83,25 +107,56 @@ class PyramidPoolBlock(Module):
         return x
 
 
-class AUXLoss(Module):
-    pass
-
-
 class Decoder(Module):
-    pass
+    def __init__(self, num_classes=1):
+        super().__init__()
+        self.conv_transpose = ConvTranspose2d(2048, 2048, 4, 2, 1)
+        self.conv = Conv2d(2048, 512, 1)
+        self.bn = BatchNorm2d(512)
+        self.activation = ReLU()
+        self.conv_transpose2 = ConvTranspose2d(512, 512, 4, 2, 1)
+        self.conv2 = Conv2d(512, num_classes, 3, 1, 1)
+
+    def forward(self, x):
+        x = self.conv_transpose(x)
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.activation(x)
+        x = self.conv_transpose2(x)
+        x = self.conv2(x)
+        return x
+
+
+class AUXLoss(Module):
+    def __init__(self, in_channels=1024, num_classes=1):
+        super().__init__()
+        self.conv = Conv2d(in_channels, 256, 3, 1, bias=False)
+        self.bn = BatchNorm2d(256)
+        self.activation = ReLU()
+        self.dropout = Dropout2d(0.1)
+        self.conv2 = Conv2d(256, num_classes, 1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.activation(x)
+        x = self.dropout(x)
+        x = self.conv2(x)
+        return x
 
 
 class PSPNetLoss(Module):
-    def __init__(self, criterion):
+    def __init__(self, aux_loss_weight=0.4, criterion=MSELoss()):
         super().__init__()
         self.criterion = criterion
+        self.aux_loss_weight = aux_loss_weight
 
-    def forward(self, losses):
-        loss, aux_loss = losses
-        loss = self.criterion(loss)
-        aux_loss = self.criterion(aux_loss)
-        return loss + 0.4 * aux_loss
+    def forward(self, predicts, ground_truth):
+        main, aux = predicts
+        aux_loss = self.criterion(aux, ground_truth)
+        main_loss = self.criterion(main, ground_truth)
+        return main_loss + self.aux_loss_weight * aux_loss
 
 
 if __name__ == '__main__':
-    summary(PSPNet(), (4, 3, 128, 128))
+    summary(PSPNet(), (4, 3, 256, 256))
