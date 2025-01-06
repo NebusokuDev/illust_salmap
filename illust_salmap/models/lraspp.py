1a5c1d7c7a513423eb15344855e84f133dac5429
sumx21t <otoro180yen@gmail.com>
2025-01-07 08:54:17 +0900

    Refactor LRASPP model by simplifying forward method.
    Removed auxiliary loss handling in the forward method, streamlining output to only the prediction tensor. Also cleaned up unused debugging code for a more concise implementation.

--- illust_salmap/models/lraspp.py
+++ illust_salmap/models/lraspp.py
@@ -11,20 +11,13 @@ class LRASPP(Module):
     def __init__(self):
         super().__init__()
         model = lraspp_mobilenet_v3_large(num_classes=1,
-                                          weights_backbone=MobileNet_V3_Large_Weights.IMAGENET1K_V2,
-                                          aux_loss=True, )
+                                          weights_backbone=MobileNet_V3_Large_Weights.IMAGENET1K_V2,)
 
         for param in model.backbone.parameters():
             param.requires_grad = False
         self.model = model
 
     def forward(self, x):
-        if self.training:
-            result = self.model(x)
-            pred = result['out']
-            aux = result['aux']
-            return pred, aux
-
         return self.model(x)['out']
 
 
@@ -33,5 +26,3 @@ if __name__ == '__main__':
     shape = (4, 3, 256, 256)
     summary(model, shape)
     benchmark(model, shape)
-
-    print(model(torch.randn(shape)))
