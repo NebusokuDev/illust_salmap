fa91284230686a2e81e3c538e0d602db8526b608
sumx21t <otoro180yen@gmail.com>
2025-01-07 09:01:20 +0900

    Modify LRASPP to support optional backbone freezing
    Added a `freeze_backbone` parameter to the LRASPP class to allow flexibility in freezing or unfreezing the backbone's parameters. This enhances customization for training workflows. Removed an unused `torch` import for cleanup.

--- illust_salmap/models/lraspp.py
+++ illust_salmap/models/lraspp.py
@@ -1,4 +1,3 @@
-import torch
 from torch.nn import Module
 from torchinfo import summary
 from torchvision.models import MobileNet_V3_Large_Weights
@@ -8,13 +7,13 @@ from illust_salmap.models.ez_bench import benchmark
 
 
 class LRASPP(Module):
-    def __init__(self):
+    def __init__(self, freeze_backbone=True,):
         super().__init__()
         model = lraspp_mobilenet_v3_large(num_classes=1,
                                           weights_backbone=MobileNet_V3_Large_Weights.IMAGENET1K_V2,)
-
-        for param in model.backbone.parameters():
-            param.requires_grad = False
+        if freeze_backbone:
+            for param in model.backbone.parameters():
+                param.requires_grad = False
         self.model = model
 
     def forward(self, x):
