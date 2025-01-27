342d68e87f52748d428ae5fe021dc12f6e1b7a94
sumx21t <otoro180yen@gmail.com>
2025-01-27 17:01:56 +0900

    fix model

--- illust_salmap/models/lraspp.py
+++ illust_salmap/models/lraspp.py
@@ -1,21 +1,23 @@
 import torch
 from torch.nn import Module
 from torchinfo import summary
-from torchvision.models import MobileNet_V3_Large_Weights
-from torchvision.models.segmentation import lraspp_mobilenet_v3_large
+from torchvision.models.segmentation import lraspp_mobilenet_v3_large, LRASPP_MobileNet_V3_Large_Weights
 
 from illust_salmap.models.ez_bench import benchmark
 from illust_salmap.training.saliency_model import SaliencyModel
 
 
 class LRASPP(Module):
-    def __init__(self, freeze_backbone=True, ):
+    def __init__(self, num_classes=1, freeze_backbone=True):
         super().__init__()
-        model = lraspp_mobilenet_v3_large(num_classes=1,
-                                          weights_backbone=MobileNet_V3_Large_Weights.IMAGENET1K_V2, )
+        model = lraspp_mobilenet_v3_large(weights=LRASPP_MobileNet_V3_Large_Weights.COCO_WITH_VOC_LABELS_V1)
         if freeze_backbone:
             for param in model.backbone.parameters():
                 param.requires_grad = False
+
+        model.classifier.high_classifier.out_channels = num_classes
+        model.classifier.low_classifier.out_channels = num_classes
+
         self.model = model
 
     def forward(self, x):
